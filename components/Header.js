975e67329f88fc74732c7681b9f43560954f9266
AliIbrahim5 <ali.alyahya514@gmail.com>
2022-09-22 00:19:11 +0300

    Building the Center of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,23 @@
 import Image from "next/image";
 import {
-    BellIcon,
-    ChatIcon,
-    ChevronDownIcon,
-    HomeIcon,
-    UserGroupIcon,
-    ViewGroupIcon,
-}   from '@heroicons/react/24/solid'
-;
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGroupIcon,
+} from "@heroicons/react/24/solid";
 import {
-   FlagIcon,
-   PlayIcon,
-   SearchIcon,
-   ShoppingCartIcon,
-}  from '@heroicons/react/24/outline'
-;
-
-
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/24/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
-     
+    <div className="sticky top-0 z-50 bg--white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
       <div className="flex items-center">
         <Image
@@ -32,12 +28,25 @@ function Header() {
         />
 
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
-            <PlayIcon className='h-6 text-gray-600 ' />
-          <input className="flex ml-2 items-center bg-transparent outline-none" type="text" placeholder="serch facebook"></input>
+          <PlayIcon className="h-6 text-gray-600 " />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="serch facebook"
+          ></input>
         </div>
       </div>
- 
+
       {/* center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* right */}
     </div>

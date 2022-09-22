40d066a8db101c6af5f0974f9cec8225ebe42c87
AliIbrahim5 <ali.alyahya514@gmail.com>
2022-09-23 01:48:52 +0300

    update from client to react

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import {getSession} from "next-auth/react"
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({session}) {
+  if (!session) return <Login/>;
   return (
     <div>
       <Head>
